2317674a1019044191ef8e56541ee2ce31a438e4
Chris Dobbyn <8264498+cdobbyn@users.noreply.github.com>
2025-11-13 17:30:05 -0800

    feat: add network private-endpoint create and delete commands

--- internal/network/privateendpoint/commands.go
+++ internal/network/privateendpoint/commands.go
@@ -37,6 +37,50 @@ func NewPrivateEndpointCommand() *cobra.Command {
   showCmd.MarkFlagRequired("name")
   showCmd.MarkFlagRequired("resource-group")
 
-  cmd.AddCommand(listCmd, showCmd)
+  createCmd := &cobra.Command{
+    Use:   "create",
+    Short: "Create a private endpoint",
+    RunE: func(cmd *cobra.Command, args []string) error {
+      name, _ := cmd.Flags().GetString("name")
+      resourceGroup, _ := cmd.Flags().GetString("resource-group")
+      location, _ := cmd.Flags().GetString("location")
+      subnetID, _ := cmd.Flags().GetString("subnet-id")
+      privateLinkResourceID, _ := cmd.Flags().GetString("private-link-resource-id")
+      groupID, _ := cmd.Flags().GetString("group-id")
+      tags, _ := cmd.Flags().GetStringToString("tags")
+      return Create(context.Background(), cmd, name, resourceGroup, location, subnetID, privateLinkResourceID, groupID, tags)
+    },
+  }
+  createCmd.Flags().StringP("name", "n", "", "Private endpoint name")
+  createCmd.Flags().StringP("resource-group", "g", "", "Resource group name")
+  createCmd.Flags().StringP("location", "l", "", "Location (e.g., eastus, westus2)")
+  createCmd.Flags().String("subnet-id", "", "Resource ID of the subnet")
+  createCmd.Flags().String("private-link-resource-id", "", "Resource ID of the private link resource")
+  createCmd.Flags().String("group-id", "", "Group ID of the private link service (e.g., sqlServer, blob)")
+  createCmd.Flags().StringToString("tags", nil, "Space-separated tags: key1=value1 key2=value2")
+  createCmd.MarkFlagRequired("name")
+  createCmd.MarkFlagRequired("resource-group")
+  createCmd.MarkFlagRequired("location")
+  createCmd.MarkFlagRequired("subnet-id")
+  createCmd.MarkFlagRequired("private-link-resource-id")
+  createCmd.MarkFlagRequired("group-id")
+
+  deleteCmd := &cobra.Command{
+    Use:   "delete",
+    Short: "Delete a private endpoint",
+    RunE: func(cmd *cobra.Command, args []string) error {
+      name, _ := cmd.Flags().GetString("name")
+      resourceGroup, _ := cmd.Flags().GetString("resource-group")
+      noWait, _ := cmd.Flags().GetBool("no-wait")
+      return Delete(context.Background(), name, resourceGroup, noWait)
+    },
+  }
+  deleteCmd.Flags().StringP("name", "n", "", "Private endpoint name")
+  deleteCmd.Flags().StringP("resource-group", "g", "", "Resource group name")
+  deleteCmd.Flags().Bool("no-wait", false, "Do not wait for the long-running operation to finish")
+  deleteCmd.MarkFlagRequired("name")
+  deleteCmd.MarkFlagRequired("resource-group")
+
+  cmd.AddCommand(listCmd, showCmd, createCmd, deleteCmd)
   return cmd
 }
